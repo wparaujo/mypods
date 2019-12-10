5dcc44ef272cece16f693b274647b0dfd1094f3f
wesley.araujo <wesley.sickert@gmail.com>
2019-12-10 11:26:20 -0300

    solve preliminar feature install command

--- lib/mypods/mypods-core/dsl.rb
+++ lib/mypods/mypods-core/dsl.rb
@@ -6,7 +6,25 @@ module MyPod
             # attr_accessor 
 
             def repo(name = nil, **requirements)
-                stdout_str, status = Open3.capture2("git clone #{requirements[:source]}")
+                # stdout_str, stdout_error, status = Open3.capture3("git clone #{requirements[:source]}")
+                root_dir = Pathname.pwd
+                
+                if Dir.exists?(name)
+                    Dir.chdir("#{name}")
+                    stdout_str, status = Open3.capture2("git pull origin #{requirements[:branch]}")
+                    # p stdout_str
+                    Dir.chdir(root_dir)
+                else
+                    stdout_str, status = Open3.capture2("git clone #{requirements[:source]}")
+                    # p stdout_str
+                    
+                    if status.success?
+                        Dir.chdir("#{name}")
+                        stdout_str, status = Open3.capture2("git fetch && git checkout -b #{requirements[:branch]} && git pull --allow-unrelated-histories origin #{requirements[:branch]}")
+                        Dir.chdir(root_dir)
+                        # p stdout_str
+                    end
+                end
             end
 
             # abstract to installer class
