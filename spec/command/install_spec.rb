e6e084199c576cf4bf419350a611fa1cf698c70a
Wesley Araujo <wparaujo@gmail.com>
2020-01-15 16:44:32 -0300

    init specs

---
+++ spec/command/install_spec.rb
@@ -0,0 +1,20 @@
+module MyPod
+    describe Command::Install do
+        it 'tells to the user that no Mypodsfile was found' do
+        end
+    end
+
+    describe 'download pods' do
+        it "dont't have pods locally"
+        end
+
+        it "have pods locally" do
+        end
+
+        it 'exists some pods' do
+        end
+
+        it "dont't found branch specified" do
+        end
+    end
+end
